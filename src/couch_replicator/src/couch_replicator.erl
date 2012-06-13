6a5127a77ec35af10938bd0424ef57b6dccc9b41
Adam Kocoloski <adam@cloudant.com>
2012-06-13 14:59:51 -0400

    Define LOWEST_SEQ

--- src/couch_replicator/src/couch_replicator.erl
+++ src/couch_replicator/src/couch_replicator.erl
@@ -24,6 +24,8 @@
 -export([init/1, terminate/2, code_change/3]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 
+-define(LOWEST_SEQ, 0).
+
 -include_lib("couchdb/include/couch_db.hrl").
 -include_lib("couch_replicator_api_wrap.hrl").
 -include_lib("couch_replicator.hrl").
